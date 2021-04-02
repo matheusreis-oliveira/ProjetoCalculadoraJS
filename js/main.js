73a8ad15433691e1123c9962d1a83590e46b797e
Matheus dos Reis <mathreis1999@gmail.com>
2021-04-02 13:14:50 -0300

    Adição do javascript

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function calcular(tipo, valor) {
+
+    if (tipo === 'acao') {
+
+        if (valor === 'c') {
+            //limpar o visor do resultado
+            document.getElementById('resultado').value = ''
+        }
+
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '÷' || valor === '.') {
+            //contatenar com o resultado para adicionar ao visor
+            document.getElementById('resultado').value += valor
+        }
+
+        if (valor === '=') {
+            //efetuar o calculo
+            var valorCampo = eval(document.getElementById('resultado').value)
+            console.log(eval(valorCampo))
+            document.getElementById('resultado').value = valorCampo
+        }
+
+    } else if (tipo === 'valor') {
+        document.getElementById('resultado').value += valor
+    }
+}
